0d4443bf0b359dcf4a769c71fd065b39134809e9
lauramiftha <131165427+lauramiftha@users.noreply.github.com>
2023-05-27 12:24:42 +0700

    install quasar

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,1 +1,50 @@
-<template>ABOUT</template>
+<template>
+    <q-page>
+      <q-page-container>
+        <h1>About Page</h1>
+        <p>This is the about page of our application.</p>
+  
+        <!-- Contoh layout tambahan -->
+        <div class="layout">
+          <h2>About Layout 1</h2>
+          <p>Ini adalah layout menarik untuk halaman About</p>
+          <q-img
+            src="/path/to/image.jpg"
+            alt="About Image"
+            class="about-image"
+          />
+        </div>
+  
+        <div class="layout">
+          <h2>About Layout 2</h2>
+          <p>Ini adalah layout lain yang menarik untuk halaman About</p>
+          <q-img
+            src="/path/to/another-image.jpg"
+            alt="Another About Image"
+            class="about-image"
+          />
+        </div>
+      </q-page-container>
+    </q-page>
+  </template>
+  
+  <script>
+  export default {
+    name: "About",
+  };
+  </script>
+  
+  <style scoped>
+  .layout {
+    margin: 20px;
+    padding: 10px;
+    border: 1px solid #ccc;
+  }
+  
+  .about-image {
+    width: 200px;
+    height: auto;
+    margin-top: 10px;
+  }
+  </style>
+  
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,1 +1,36 @@
-<template>HOME</template>
+<template>
+    <q-page>
+      <q-page-container>
+        <h1>Welcome to Home Page</h1>
+        <div>
+          <router-link to="/about">Go to About Page</router-link>
+        </div>
+  
+        <!-- Contoh layout tambahan -->
+        <div class="layout">
+          <h2>Layout 1</h2>
+          <p>Ini adalah layout 1</p>
+        </div>
+  
+        <div class="layout">
+          <h2>Layout 2</h2>
+          <p>Ini adalah layout 2</p>
+        </div>
+      </q-page-container>
+    </q-page>
+  </template>
+  
+  <script>
+  export default {
+    name: "Home",
+  };
+  </script>
+  
+  <style scoped>
+  .layout {
+    margin: 20px;
+    padding: 10px;
+    border: 1px solid #ccc;
+  }
+  </style>
+  
