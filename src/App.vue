7e69e4d656cd60f46bc532dcd589d785b863f5b8
lauramiftha <131165427+lauramiftha@users.noreply.github.com>
2023-05-27 11:00:12 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
 <template>
   <q-layout view="hHh lpR fFf">
-
     <q-header elevated class="bg-primary text-white" height-hint="98">
       <q-toolbar>
         <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
 
         <q-toolbar-title>
           <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
           </q-avatar>
           Title
         </q-toolbar-title>
@@ -16,9 +15,10 @@
       </q-toolbar>
 
       <q-tabs align="left">
-        <q-route-tab to="/page1" label="Page One" />
-        <q-route-tab to="/page2" label="Page Two" />
+        <q-route-tab to="/" label="Home" />
+        <q-route-tab to="/about" label="About" />
         <q-route-tab to="/page3" label="Page Three" />
+        <q-route-tab to="/page4" label="Page Four" />
       </q-tabs>
     </q-header>
 
@@ -31,42 +31,34 @@
     </q-drawer>
 
     <q-page-container>
-      <router-view />
+      <div class="q-ma-lg">
+        <router-view />
+      </div>
     </q-page-container>
 
     <q-footer elevated class="bg-grey-8 text-white">
       <q-toolbar>
         <q-toolbar-title>
           <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
           </q-avatar>
           <div>Title</div>
         </q-toolbar-title>
       </q-toolbar>
     </q-footer>
-
   </q-layout>
 </template>
 
-<script>
-import { ref } from 'vue'
+<script setup>
+import { ref } from "vue";
 
-export default {
-  setup () {
-    const leftDrawerOpen = ref(false)
-    const rightDrawerOpen = ref(false)
+const leftDrawerOpen = ref(false);
+const rightDrawerOpen = ref(false);
 
-    return {
-      leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      },
-
-      rightDrawerOpen,
-      toggleRightDrawer () {
-        rightDrawerOpen.value = !rightDrawerOpen.value
-      }
-    }
-  }
-}
+const toggleLeftDrawer = () => {
+  leftDrawerOpen.value = !leftDrawerOpen.value;
+};
+const toggleRightDrawer = () => {
+  rightDrawerOpen.value = !rightDrawerOpen.value;
+};
 </script>
--- src/main.js
+++ src/main.js
@@ -1,23 +1,27 @@
-// FILE: main.js
-
-import { createApp } from 'vue'
-import { Quasar } from 'quasar'
+import { createApp } from "vue";
+import { Quasar, Notify } from "quasar";
+import router from "./router";
 
 // Import icon libraries
-import '@quasar/extras/material-icons/material-icons.css'
+import "@quasar/extras/material-icons/material-icons.css";
 
 // Import Quasar css
-import 'quasar/src/css/index.sass'
+import "quasar/src/css/index.sass";
 
 // Assumes your root component is App.vue
 // and placed in same folder as main.js
-import App from './App.vue'
+import App from "./App.vue";
 
-const myApp = createApp(App)
+const myApp = createApp(App);
+
+myApp.use(router);
 
 myApp.use(Quasar, {
-  plugins: {}, // import Quasar plugins and add here
-})
+  plugins: { Notify },
+  config: {
+    notify: {},
+  },
+});
 
 // Assumes you have a <div id="app"></div> in your index.html
-myApp.mount('#app')
+myApp.mount("#app");
